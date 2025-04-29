eb9046b524368ddf9252c5c835cf608b5569011d
math3wsl3vi <levimathews9841@gmail.com>
2025-04-29 23:06:02 +0300

    boop

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 const nextConfig = {
-  output: 'standalone',
   experimental: {
-    serverActions: {}, 
-    appDir: true,
-  },
-  transpilePackages: [
-    // Add any npm packages that need transpilation here
-  ],
-  async rewrites() {
-    return [
-      // Add any URL rewrites if needed
-    ];
+    serverActions: {} 
   }
 };
 
